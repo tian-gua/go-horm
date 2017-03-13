43864f53a07afee034cc72c46f170cee7829fdb6
叶浩 <77729052@qq.com>
2017-03-13 17:36:42 +0800

    .

--- horm/horm_test.go
+++ horm/horm_test.go
@@ -6,28 +6,56 @@ import (
 )
 
 func TestHorm(t *testing.T) {
-	hormManager := New() //创建一个HormManager
+	//创建一个HormManager
+	hormManager := New()
 	did, err := hormManager.Connect("127.0.0.1", 3306, "root", "root", "horm")
 	dealError(err)
 	t.Logf("did=%d", did)
+
+	//创建一个horm
 	horm := hormManager.Create(did)
+
+	//开始一个事务
 	err = horm.Begin()
 	dealError(err)
+
+	//创建一个测试struct
 	th := newTestHorm()
+
+	//保存新建的struct
 	res, err := horm.Save(th)
 	dealError(err)
 	lastId, err := res.LastInsertId()
 	dealError(err)
+
+	//删除新建的struct
 	th.Id = int(lastId)
 	rows, err := horm.DelById(th)
 	dealError(err)
 	t.Logf("rows=%d", rows)
+
+	//查询id = 9的struct
 	th2 := &testHorm{Id: 9}
 	err = horm.FindById(th2)
 	dealError(err)
 	t.Logf("%+v", th2)
+
+	th2.Description = "更新horm"
+	rows, err = horm.UpdateById(th2)
+	dealError(err)
+	t.Logf("更新了[%d]条记录", rows)
+
+	//查询id = 9的struct
+	th2 = &testHorm{Id: 9}
+	err = horm.FindById(th2)
+	dealError(err)
+	t.Logf("%+v", th2)
+
+	//提交事务
 	err = horm.Commit()
 	dealError(err)
+
+	//关闭所有连接
 	err = hormManager.CloseAll()
 	dealError(err)
 }
